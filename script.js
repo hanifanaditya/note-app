6342963084943bd963813ffca9e848c3670bc8a2
Hanifan Aditya <hanifanaditya34@gmail.com>
2023-06-04 18:50:34 +0700

    membuat tombol dan membuat textarea

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const create = document.querySelector('.Create');
-const container = document.querySelector('.container');
+const create = document.querySelector('.create');
+const section = document.querySelector('section');
 
+create.addEventListener('click',() => {
+    section.style.display = 'flex'
+    let hours = new Date().getHours();
+    let minutes = new Date().getMinutes();
+    let p = document.createElement('p');
+    p.innerHTML = `Diedit ${hours}:${minutes}`;
+    let span = document.createElement('span');
+    span.innerHTML = '&#129120;'
+    span.appendChild(p);
+    section.appendChild(span);
+
+    let textarea = document.createElement('textarea');
+    section.appendChild(textarea);
+    create.style.display = 'none';
+})
